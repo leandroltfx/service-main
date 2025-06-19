c58628b4f2d71396677257519841f9d13d8e5ea5
leandroltfx <leandroltfbr@hotmail.com>
2025-06-19 08:23:45 -0300

    init

---
+++ src/main/java/com/rocketseat/service_main/ServiceMainApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.service_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/service_main/ServiceMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.service_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
