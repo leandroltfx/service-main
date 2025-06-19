8d7bfa2099bc551e0465123d34b7cb47f86d5e8a
leandroltfx <leandroltfbr@hotmail.com>
2025-06-19 09:25:11 -0300

    adicionando configurações de servidor eureka e servidor de configuração

--- src/main/java/com/rocketseat/service_main/ServiceMainApplication.java
+++ src/main/java/com/rocketseat/service_main/ServiceMainApplication.java
@@ -2,8 +2,14 @@ package com.rocketseat.service_main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// Identifica para o Spring que essa aplicação é um servidor Eureka
+@EnableEurekaServer
+// Identifica para o Spring que essa aplicação é um servidor de configuração (Spring Cloud Config Server)
+@EnableConfigServer
 public class ServiceMainApplication {
 
 	public static void main(String[] args) {
